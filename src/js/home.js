fa7bf786223ec8ed610a8bcab961ace99f5a5f06
Yuji Lai <ylai9927@gmail.com>
2017-10-22 12:03:02 -0700

    switch page

--- src/js/home.js
+++ src/js/home.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
+
+    $(document).click(function() {
+        if($(document).hasClass('slide-up')) {
+          $(document).addClass('slide-down', 10000, 'easeOutBounce');
+          $(document).removeClass('slide-up');
+        } else {
+          $(document).removeClass('slide-down');
+          $(document).addClass('slide-up', 10000, 'easeOutBounce');
+        }
+    });
+
   $(document).click(function(e){
     //animation
     if (e.button == 0 ) {
       window.location.href="./index.html";
     }
   });
+
 });
